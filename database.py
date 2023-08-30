3154c7c42ab4efa3d5fa24dabffaf8bb237fcd3a
Radwa <radwaayman2004@gmail.com>
2023-08-31 01:03:14 +0300

    Added history, info, update commands.

--- database.py
+++ database.py
@@ -1,19 +1,33 @@
 import sqlite3
 
-connection = sqlite3.connect('reverts_database.db')
+connection = sqlite3.connect("db/reverts_database.db")
 cursor = connection.cursor()
 cursor.execute(
-    '''
-    create table if not exists members(
-        id int primary key,
-        member_id int not null,
-        member_name text not null,
-        gender text not null,
-        author text not null,
-        date text not null,
-        notes text
-    )
-    ''')
-connection.commit()
+    """
+    CREATE TABLE IF NOT EXISTS REVERTS (
+        id INTEGER PRIMARY KEY,
+        revert_id INTEGER NOT NULL,
+        gender TEXT NOT NULL,
+        mod_id INTEGER NOT NULL,
+        date TEXT NOT NULL,
+        notes TEXT
+    );
+"""
+)
 
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS HISTORY (
+        id INTEGER PRIMARY KEY,
+        revert_id INTEGER NOT NULL,
+        mod_id INTEGER NOT NULL,
+        date TEXT NOT NULL,
+        notes TEXT,
+        FOREIGN KEY (revert_id) REFERENCES REVERTS (revert_id),
+        FOREIGN KEY (mod_id) REFERENCES REVERTS (mod_id)
+    );
+"""
+)
+
+connection.commit()
 connection.close()
--- main.py
+++ main.py
@@ -1,51 +1,129 @@
 import discord
 from discord.ext import commands
-import datetime
+from datetime import datetime
 import sqlite3
+import aiosql
 
 
-connection = sqlite3.connect('db/reverts_database.db')
-cursor = connection.cursor()
+connection = sqlite3.connect("db/reverts_database.db")
+cr = connection.cursor()
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 # @bot.event
 # async def on_ready():
 #    print(f'Logged in as {bot.user.name}')
 
-# cursor.execute(
-#         f'''
-#             INSERT INTO members (member_id, member_name, gender, author, notes, date) VALUES (?, ?, ?, ?, ?, datetime('now'));
-#         ''',( 124, "meow", "Male" , "ffiwen", "fioewhbfiew")
-#     )
-
-
-@bot.command()
+"""@bot.command()
 async def show(ctx: commands.Context, role : discord.Role):
     for mem in ctx.guild.members:
         if role in mem.roles:
             await ctx.send(mem.id)
+"""
+
 
 @bot.command()
 async def add(ctx: commands.Context, mem: discord.Member, *, notes=None):
     is_male = discord.utils.get(mem.roles, id=1146032099854389249) != None
     username = mem.global_name or str(mem)
-    author_username = ctx.author.global_name or str(ctx.author)
+    cr.execute(
+        f"""
+        SELECT * FROM REVERTS WHERE revert_id = ? ;
+        """,
+        (mem.id,),
+    )
+    row = cr.fetchone()
 
-    cursor.execute(
-        f'''
-            INSERT INTO members (member_id, member_name, gender, author, notes, date) VALUES (?, ?, ?, ?, ?, datetime('now'));
-        ''',( mem.id, username, "Male" if is_male else "Female", author_username, notes)
-    ) 
+    if row is not None:
+        embed = discord.Embed(
+            title="This user is already recorded! ", color=discord.Color.red()
+        )
+        await ctx.send(embed=embed)
+
+        return
+
+    cr.execute(
+        f"""
+            INSERT INTO REVERTS (revert_id, gender, mod_id, notes, date) VALUES (?, ?, ?, ?, datetime('now'));
+        """,
+            (mem.id, "Male" if is_male else "Female", ctx.author.id, notes),
+    )
+    cr.execute("INSERT INTO HISTORY (revert_id, mod_id, notes, date) VALUES (?, ?, ?, datetime('now'));", (mem.id, ctx.author.id, notes))
     connection.commit()
 
-def get_gender_from_roles(user):
-    for role in user.roles:
-        if role.name.lower() in ["male", "female", "non-binary", "other"]:
-            return role.name
-    return "Unknown"
 
-bot.run('MTE0Mzg5OTM3MDI0MTEzODgzMg.Ga6GZx.-AnMH13r2hg7-16U6T8rwYV8XsC0xbwZWHYLY8')
+@bot.command()
+async def info(ctx: commands.Context, mem: discord.Member):
+    cr.execute("SELECT * FROM REVERTS WHERE revert_id = ?;", (mem.id,))
+    row = cr.fetchone()
+
+    fmt_date = discord.utils.format_dt(datetime.fromisoformat(row[4]), style="D")
+
+    if row:  # print (row)
+        embed = discord.Embed(title="User Information: ", color=discord.Color.blue())
+        embed.add_field(name="User : ", value=f"<@{row[1]}>", inline=                                                                                                               True)
+        embed.add_field(name="Gender: ", value=row[2], inline=False)
+        embed.add_field(name="Main mod in contact: ", value=f"<@{row[3]}>", inline=True)
+        embed.add_field(name="Date of last follow up: ", value=fmt_date, inline=False)
+        embed.add_field(name="Follow-up notes: ", value=row[5], inline=False)
+
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="the user is not found! ", color=discord.Color.red()
+        )
+        await ctx.send(embed=embed)
+
+
+@bot.command()
+async def update(ctx: commands.Context, mem: discord.Member, *, new_notes=None):
+    cr.execute("SELECT * FROM REVERTS WHERE revert_id = ?", (mem.id,))
+    row = cr.fetchone()
+    if row is None:
+        print("Couldn't find")
+        return
+
+   
+    new_notes = new_notes or row[5]
+    cr.execute(
+        f"""
+            UPDATE REVERTS 
+            SET mod_id = ? , notes = ? ,date = datetime('now')
+            WHERE revert_id = ? 
+         """,
+        (
+            ctx.author.id,
+            new_notes,
+            mem.id,
+        ),
+    )
+    cr.execute(
+        "INSERT INTO HISTORY (revert_id, mod_id, notes, date) VALUES (?,?,?, datetime('now'))",
+        (mem.id, ctx.author.id, new_notes),
+    )
+
+    connection.commit()
+
+
+@bot.command()
+async def history(ctx: commands.Context, mem: discord.Member):
+    cr.execute("SELECT mod_id, date, notes FROM HISTORY WHERE revert_id = ?", (mem.id, ))
+
+    rows = cr.fetchall()
+
+    embed = discord.Embed(title=f"History for")
+    embed.colour = discord.Colour.from_rgb(255, 255, 255)
+    embed.description = f"<@{mem.id}>"
+    for row in rows:
+        mod_id, date, notes = row
+        date = discord.utils.format_dt(datetime.fromisoformat(date), style="D")
+        embed.add_field(name="Mod", value=f"<@{mod_id}> at {date}")
+        embed.add_field(name="Note", value=notes, inline=False)
+
+    await ctx.send(embed=embed)
+
+
+bot.run("MTE0Mzg5OTM3MDI0MTEzODgzMg.Ga6GZx.-AnMH13r2hg7-16U6T8rwYV8XsC0xbwZWHYLY8")
